26aa17e4c382c3098deaf9bbbff6b1dba677075f
maxwellkylee <maxwellkyle29@gmail.com>
2025-03-22 18:06:17 +0800

    icon update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" href="./img/jane_plain.png">
+    <link rel="icon" href="./img/jane-circle-icon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listahan</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
